85897eaa1b86e92be6317feb7cd9044f458ff628
atharv866 <atharvdwivedi201311@gmail.com>
2025-11-21 10:25:02 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,37 @@
+let Degrees = 0
+basic.forever(function () {
+    Degrees = input.compassHeading()
+    if (Degrees < 45 || Degrees > 315) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (Degrees < 135) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (Degrees < 225) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+})
